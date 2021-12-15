3096ad4b017c365479045d9f5aec61b2015124d9
Paul Javaloyes <90237037+PaulJDev@users.noreply.github.com>
2021-12-15 14:51:13 +0100

    Add challenge 15

---
+++ 15_El salto perfecto/index.js
@@ -0,0 +1,17 @@
+function checkSledJump(heights) {
+  for (let i = 1, j = heights.length - 1; i < heights.length/2; i++, j--) {
+    if(heights[i - 1] >= heights[i] || heights[j - 1] <= heights[j]) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(checkSledJump([1, 2, 3, 2, 1])) // true: sube y baja de forma estricta
+console.log(checkSledJump([0, 1, 0])) // -> true: sube y baja de forma estricta
+console.log(checkSledJump([0, 3, 2, 1])) // -> true: sube y baja de forma estricta
+console.log(checkSledJump([0, 1000, 1])) // -> true: sube y baja de forma estricta
+
+console.log(checkSledJump([2, 4, 4, 6, 2])) // false: no sube de forma estricta
+console.log(checkSledJump([1, 2, 3])) // false: sólo sube
+console.log(checkSledJump([1, 2, 3, 2, 1, 2, 3])) // false: sube y baja y sube... ¡no vale!

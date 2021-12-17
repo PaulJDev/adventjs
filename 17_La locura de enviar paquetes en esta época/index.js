28ad70cfa88a70f51ce295243e1f205b0f9bdd4d
Paul Javaloyes <90237037+PaulJDev@users.noreply.github.com>
2021-12-17 20:11:19 +0100

    Add challenge 17

---
+++ 17_La locura de enviar paquetes en esta época/index.js
@@ -0,0 +1,27 @@
+function countPackages(carriers, carrierID) {
+    const [, maxItems, team ] = carriers.find(carrier => carrier[0] === carrierID);
+  
+    return team.reduce((t, c) => t + countPackages(carriers, c), 0) + maxItems
+}
+
+const carriers = [
+    ['dapelu', 5, ['midu', 'jelowing']],
+    ['midu', 2, []],
+    ['jelowing', 2, []]
+]
+  
+console.log(countPackages(carriers, 'dapelu')) // 9
+// 5 de dapelu, 2 de midu y 2 de jelowing = 9
+  
+const carriers2 = [
+    ['lolivier', 8, ['camila', 'jesuspoleo']],
+    ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+    ['jesuspoleo', 4, []],
+    ['sergiomartinez', 4, []],
+    ['conchaasensio', 3, ['facundocapua', 'faviola']],
+    ['facundocapua', 2, []],
+    ['faviola', 1, []]
+]
+  
+console.log(countPackages(carriers2, 'camila')) // 15
+// 5 de camila, 4 de sergiomartinez, 3 de conchaasensio, 2 de facundocapua y 1 de faviola = 15

192a295ff9e82b5e3c94a339eb4bccd82aca0422
Paul Javaloyes <90237037+PaulJDev@users.noreply.github.com>
2021-12-23 19:13:46 +0100

    Add challenge 23

---
+++ 23_Puedes reconfigurar las fábricas para no parar de crear regalos/index.js
@@ -0,0 +1,57 @@
+function canReconfigure(from, to) {
+    const crossData = (str1, str2) => {
+        return [...str1].reduce((a, c, i) => {
+            if (c !== str2[i]) a[c] = str2[i]
+            return a
+        }, {})
+    }
+
+    return Object.keys(crossData(from, to)).length === Object.keys(crossData(to, from)).length
+}
+  
+let from = 'BAL'
+let to   = 'LIB'
+console.log(canReconfigure(from, to)) // true
+/* la transformación sería así:
+B -> L
+A -> I
+L -> B
+*/
+
+from = 'CON'
+to   = 'JUU'
+console.log(canReconfigure(from, to)) // false
+/* no se puede hacer la transformación:
+C -> J
+O -> U
+N -> FALLO
+*/
+
+from = 'MMM'
+to   = 'MID'
+console.log(canReconfigure(from, to)) // false
+/* no se puede hacer la transformación:
+M -> M (BIEN, asigna el mismo carácter a si mismo)
+M -> I (FALLO, asigna el mismo carácter a dos letras distintas)
+M -> D (FALLO, asigna el mismo carácter a dos letras distintas)
+*/
+
+from = 'AA'
+to   = 'MID'
+console.log(canReconfigure(from, to)) // false -> no tiene la misma longitud
+
+from = 'xxxMMM'
+to   = 'aaaDDD'
+console.log(canReconfigure(from, to)) //true
+
+from = 'aaaDDDmmm'
+to   = 'aaaDDDmmm'
+console.log(canReconfigure(from, to)) //true
+
+from = 'xxxqqqeeefffggg'
+to   = 'xxxqqqeeefffgg'
+console.log(canReconfigure(from, to)) //false
+
+from = 'abcdefghijkl'
+to   = 'lkjihgfedcba'
+console.log(canReconfigure(from, to)) //true

7f2cf3b109df3a7f5857de1458a8a51d170b29b9
Paul Javaloyes <90237037+PaulJDev@users.noreply.github.com>
2021-12-24 15:33:08 +0100

    Add challenge 24

---
+++ 24_Comparando árboles de Navidad/index.js
@@ -0,0 +1,25 @@
+function checkIsSameTree(treeA, treeB) {
+    return (
+        !(treeA && treeB) ||
+        treeA.value === treeB.value &&
+        checkIsSameTree(treeA.left, treeB.left) &&
+        checkIsSameTree(treeA.right, treeB.right)    
+    )
+}
+
+const tree = {
+    value: 1,
+    left: { value: 2, left: null, right: null },
+    right: { value: 3, left: null, right: null }
+}
+  
+console.log(checkIsSameTree(tree, tree)) // true
+
+const tree2 = {
+value: 1,
+left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
+right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
+}
+  
+console.log(checkIsSameTree(tree, tree2)) // false
+console.log(checkIsSameTree(tree2, tree2)) // true
